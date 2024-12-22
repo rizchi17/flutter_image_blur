5c0ba427af15f2a625587a2c2c69822ebe9b5fe1
rizchi17 <rizchi17ub@gmail.com>
2024-12-22 22:42:29 +0900

    flutter_blurhashを試した

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:blur/blur.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:image/image.dart' as img;
 import 'package:path_provider/path_provider.dart';
 
@@ -58,63 +59,72 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              color: Colors.black12,
-              alignment: Alignment.center,
-              width: 400,
-              height: 400,
-              child: blurredImageFile == null
-                  ? loading
-                      ? const SizedBox(
-                          width: 40,
-                          height: 40,
-                          child: CircularProgressIndicator(),
-                        )
-                      : Image.asset('images/sample.jpg').blurred(blur: blurLevel, colorOpacity: 0)
-                  : Image.file(blurredImageFile!),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    if (blurLevel != 0) {
-                      blurLevel = 0;
-                    } else {
-                      blurLevel = 5;
-                    }
-                  });
-                },
-                child: const Text('blur')),
-            ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  loading = true;
-                });
-                final file = await copyAssetToLocal('images/sample.jpg');
-                final data = file.readAsBytesSync();
-                final img.Image? image = img.decodeImage(data);
-                if (image != null) {
-                  final blurredImage = img.gaussianBlur(image, radius: 40);
-                  final tempDir = await getTemporaryDirectory();
-                  final tempFile = File('${tempDir.path}/blurred_image.jpg');
-                  tempFile.writeAsBytesSync(img.encodeJpg(blurredImage));
-                  setState(() {
-                    blurredImageFile = tempFile;
-                  });
-                }
-              },
-              child: const Text('image gaussianBlur'),
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                color: Colors.black12,
+                alignment: Alignment.center,
+                width: 400,
+                height: 400,
+                child: blurredImageFile == null
+                    ? loading
+                        ? const SizedBox(
+                            width: 40,
+                            height: 40,
+                            child: CircularProgressIndicator(),
+                          )
+                        : Image.asset('images/sample.jpg').blurred(blur: blurLevel, colorOpacity: 0)
+                    : Image.file(blurredImageFile!),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (blurLevel != 0) {
+                        blurLevel = 0;
+                      } else {
+                        blurLevel = 5;
+                      }
+                    });
+                  },
+                  child: const Text('blur')),
+              ElevatedButton(
+                onPressed: () async {
+                  setState(() {
+                    loading = true;
+                  });
+                  final file = await copyAssetToLocal('images/sample.jpg');
+                  final data = file.readAsBytesSync();
+                  final img.Image? image = img.decodeImage(data);
+                  if (image != null) {
+                    final blurredImage = img.gaussianBlur(image, radius: 40);
+                    final tempDir = await getTemporaryDirectory();
+                    final tempFile = File('${tempDir.path}/blurred_image.jpg');
+                    tempFile.writeAsBytesSync(img.encodeJpg(blurredImage));
+                    setState(() {
+                      blurredImageFile = tempFile;
+                    });
+                  }
+                },
+                child: const Text('image gaussianBlur'),
+              ),
+              const SizedBox(
+                width: 300,
+                height: 200,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    BlurHash(hash: "L5H2EC=PM+yV0g-mq.wG9c010J}I"),
+                    Text('おまけ:画像ファイルのblurhash値があればflutter_blurhashを使ってblurを表示できる(blurhash値の生成はできない)'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
