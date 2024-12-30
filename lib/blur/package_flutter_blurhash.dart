0f48c8ee93660da0faf7426762edeacb787784ac
rizchi17 <rizchi17ub@gmail.com>
2024-12-30 11:10:58 +0900

    channelを変更できるようにした

--- lib/blur/package_flutter_blurhash.dart
+++ lib/blur/package_flutter_blurhash.dart
@@ -9,7 +9,20 @@ class PackageFlutterBlurhash extends StatefulWidget {
 }
 
 class _PackageFlutterBlurhashState extends State<PackageFlutterBlurhash> {
-  final String hash = "LLJj@?MxT}R+4o%LIUtRY6of9FoJ";
+  final String hash = "LKO2?U%2Tw=w]~RBVZRi};RPxuwH";
+  final Color blurHashColor = Colors.blue;
+  bool showBlurHash = false; // BlurHashを表示するフラグ
+
+  @override
+  void initState() {
+    super.initState();
+    // 遅延してBlurHashを表示
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        showBlurHash = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +46,43 @@ class _PackageFlutterBlurhashState extends State<PackageFlutterBlurhash> {
             SizedBox(
               width: 300,
               height: 200,
-              child: BlurHash(hash: hash),
+              child: showBlurHash
+                  ? BlurHash(
+                      hash: hash,
+                      color: blurHashColor,
+                      decodingHeight: 10,
+                      decodingWidth: 10,
+                      image: 'https://picsum.photos/250?image=9',
+                      onDecoded: () {
+                        print('onDecoded');
+                      },
+                      onStarted: () {
+                        print('onStarted');
+                      },
+                      onDisplayed: () {
+                        print('onDisplayed');
+                      },
+                      onReady: () {
+                        print('onReady');
+                      },
+                      duration: const Duration(milliseconds: 3000), // 表示アニメーションの時間
+                    )
+                  : Center(
+                      child: Text(
+                        'デコード待機中...',
+                        style: TextStyle(color: blurHashColor),
+                      ),
+                    ),
+            ),
+            Container(
+              width: 100,
+              height: 50,
+              color: blurHashColor,
+              alignment: Alignment.center,
+              child: const Text(
+                'BlurHash色',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             const Text('補足: blurhash値の生成はできないのでhttps://blurha.sh/などを利用する必要あり'),
           ],
